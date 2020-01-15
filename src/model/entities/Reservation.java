a5ac743d5c7165f677d128300e767b216674db73
alexandremolica <alexandremolica@gmail.com>
2020-01-15 18:29:39 -0300

    Solução boa - Good solution - acerto sem throws na assint do constructor

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -14,7 +14,7 @@ public class Reservation {
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut){
 		if ( !checkOut.after(checkIn) ){
 			throw new DomainException("Check-out date must be after check-in date");
 		}
